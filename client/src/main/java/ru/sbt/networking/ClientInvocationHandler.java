7f6bd1819eff426a796c192b0541d549006112d2
Alexander Ushakov <a.ushakov.mipt@gmail.com>
2016-09-10 08:57:31 +0300

    Split serialization module into server and client respective modules.

--- client/src/main/java/ru/sbt/networking/ClientInvocationHandler.java
+++ client/src/main/java/ru/sbt/networking/ClientInvocationHandler.java
@@ -5,6 +5,9 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.net.Socket;
 
+import static ru.sbt.networking.ClientSerialization.deserialize;
+import static ru.sbt.networking.ClientSerialization.serialize;
+
 public class ClientInvocationHandler implements InvocationHandler {
     private final String host;
     private final int port;
@@ -17,8 +20,8 @@ public class ClientInvocationHandler implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Exception {
         try (Socket socket = new Socket(host, port)) {
-            ClientSerialization.serialize(method, args, socket);
-            return ClientSerialization.deserialize(socket);
+            serialize(method, args, socket);
+            return deserialize(socket);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
